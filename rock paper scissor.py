eac00f762d216e83b9ae5f5121fd31ed4051ecc6
Vextoriinn <157535822+Vextoriinn@users.noreply.github.com>
2024-01-25 11:19:41 -0500

    Add files via upload

---
+++ rock paper scissor.py
@@ -0,0 +1,27 @@
+
+from random import randint
+
+t = ["rock", "paper", "scissors"]
+
+computer = t[randint(0,2)]
+
+player = input("rock, paper, scissors?")
+if player == computer:
+        print("tie")
+elif player == "rock":
+    if computer == "paper":
+            print("You lose", computer, "covers", player)
+    else:
+            print("you win!", player, "smashes", computer)
+elif player == "paper":
+    if computer == "scissor":
+            print("You lose", computer, "cuts", player)
+    else:
+            print("you win!", player, "covers", computer)    
+elif player == "scissor":
+        if computer == "rock":
+                print("You win", computer, "smashes", player)
+        else:
+                print("you lose!", player, "cuts", computer)    
+else:
+        print("invalid play, pick inside index.")  
